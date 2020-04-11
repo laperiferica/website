5c7f792f38c5765cae53b9d331e5e706499d9b79
Javier López Úbeda <jlopezcur@gmail.com>
2020-04-11 18:43:45 +0200

    Update menu

---
+++ src/components/Menu/Burger.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const StyledBurger = styled.button`
+  @media (max-width: 734px) {
+    & {
+      display: flex;
+    }
+  }
+  display: none;
+  flex-direction: column;
+  justify-content: space-around;
+  width: 2rem;
+  height: 2rem;
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  padding: 0;
+  margin: 0 auto;
+
+  &:focus {
+    outline: none;
+  }
+
+  div {
+    width: 2rem;
+    height: 0.25rem;
+    background: #000000;
+    border-radius: 10px;
+    transition: all 0.3s linear;
+    position: relative;
+    transform-origin: 1px;
+
+    :first-child {
+      transform: ${({ open }) => (open ? 'rotate(45deg)' : 'rotate(0)')};
+    }
+
+    :nth-child(2) {
+      opacity: ${({ open }) => (open ? '0' : '1')};
+      transform: ${({ open }) => (open ? 'translateX(20px)' : 'translateX(0)')};
+    }
+
+    :nth-child(3) {
+      transform: ${({ open }) => (open ? 'rotate(-45deg)' : 'rotate(0)')};
+    }
+  }
+`;
+
+const Burger = ({ open, setOpen }) => {
+  return (
+    <StyledBurger open={open} onClick={() => setOpen(!open)}>
+      <div />
+      <div />
+      <div />
+    </StyledBurger>
+  );
+};
+
+Burger.propTypes = {
+  open: PropTypes.bool,
+  setOpen: PropTypes.func,
+};
+
+export default Burger;
---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { Link } from 'gatsby';
+
+import Burger from './Burger';
+
+const links = [
+  { title: 'Proyectos', to: '/#proyectos' },
+  { title: 'Quienes somos', to: '/#quienes-somos' },
+  { title: 'Equipo', to: '/#equipo' },
+  { title: 'Contacta', to: '/#contacta' },
+];
+
+const StyledMenu = styled.nav`
+  min-height: 72px;
+  box-sizing: border-box;
+  .menu {
+    padding: 1rem;
+    width: 100%;
+    top: 0;
+    z-index: 1;
+    background: white;
+    text-transform: uppercase;
+    font-size: 1.3rem;
+    .links {
+      display: flex;
+      justify-items: center;
+      justify-content: center;
+      align-items: center;
+      word-wrap: wrap;
+      a {
+        display: inline-block;
+        padding: 0.5rem 1rem;
+      }
+    }
+    @media (max-width: 734px) {
+      .links {
+        display: ${(props) => (props.open ? 'flex' : 'none')};
+        flex-direction: column;
+        margin-top: 1rem;
+      }
+    }
+  }
+`;
+
+const fn = () => {
+  const el = document.querySelector('.menu');
+  if (el.style.position === 'fixed') {
+    if (window.scrollY < 400) {
+      el.style.position = 'unset';
+      el.style.borderBottom = 'none';
+    }
+  } else {
+    if (window.scrollY > 400) {
+      el.style.position = 'fixed';
+      el.style.borderBottom = '2px solid black';
+    }
+  }
+};
+
+const Menu = () => {
+  const [open, setOpen] = useState(false);
+  useEffect(() => {
+    window.addEventListener('scroll', fn);
+    return () => {
+      window.removeEventListener('scroll', fn);
+    };
+  });
+  return (
+    <StyledMenu open={open}>
+      <div className={'menu'}>
+        <Burger open={open} setOpen={setOpen} />
+        <div className={`links`}>
+          {links.map((x, idx) => (
+            <Link key={idx} to={x.to} onClick={() => setOpen(false)}>
+              {x.title}
+            </Link>
+          ))}
+        </div>
+      </div>
+    </StyledMenu>
+  );
+};
+
+export default Menu;
