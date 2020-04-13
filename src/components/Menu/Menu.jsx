498a921e979e43ad8b0bfcf5c76a92a242d5baae
Javier López Úbeda <jlopezcur@gmail.com>
2020-04-14 00:42:33 +0200

    Added some effect to menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -29,8 +29,27 @@ const StyledMenu = styled.nav`
       align-items: center;
       word-wrap: wrap;
       a {
+        // https://css-tricks.com/having-fun-with-link-hover-effects/
+        background: linear-gradient(
+          to bottom,
+          var(--mainColor) 0%,
+          var(--mainColor) 100%
+        );
+        background-position: 0 100%;
+        background-repeat: repeat-x;
+        background-size: 3px 3px;
         display: inline-block;
         padding: 0.5rem 1rem;
+        margin: 0 1rem;
+        text-decoration: none;
+        transition: background-size 0.2s;
+        &:hover {
+          // background-image: url("data:image/svg+xml;charset=utf8,<svg id='squiggle-link' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' xmlns:ev='http://www.w3.org/2001/xml-events' viewBox='0 0 20 4'><style type='text/css'>.squiggle{animation:shift .3s linear infinite;}@keyframes shift {from {transform:translateX(0);}to {transform:translateX(-20px);}}</style><path fill='none' stroke='#ff9800' stroke-width='2' class='squiggle' d='M0,3.5 c 5,0,5,-3,10,-3 s 5,3,10,3 c 5,0,5,-3,10,-3 s 5,3,10,3'/></svg>");
+          background-position: 0 100%;
+          background-size: 3px 40px;
+          background-repeat: repeat-x;
+          text-decoration: none;
+        }
       }
     }
     @media (max-width: 734px) {
@@ -53,7 +72,7 @@ const fn = () => {
   } else {
     if (window.scrollY > 400) {
       el.style.position = 'fixed';
-      el.style.borderBottom = '2px solid black';
+      el.style.borderBottom = '1px solid lightgrey';
     }
   }
 };
