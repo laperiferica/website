167fc499017474f9811a9fca883575f8f582aa11
Javier López Úbeda <jlopezcur@gmail.com>
2020-04-19 00:18:50 +0200

    Change text for who home section

--- src/components/home/Who.jsx
+++ src/components/home/Who.jsx
@@ -8,15 +8,12 @@ const StyledWho = styled.section`
 
 const Who = () => (
   <StyledWho>
-    LA PERIFÉRICA - Cultura Contemporánea es un Colectivo de Gestorxs Culturales
-    almerienses que busca poner en valor la vida cultural de la ciudad y de la
-    provincia desde una visión innovadora, apoyando y fomentando el talento
-    local, el intercambio y la cooperación internacional y sobre todo la
-    implicación y participación ciudadana a través de proyectos artísticos y
-    culturales que promuevan la reflexión de los retos y temas contemporáneos.
-    <br />
-    Uno de nuestros ámbitos de trabajo son los proyectos internacionales como
-    los que realizamos a través del programa europeo Erasmus +.
+    Somos un Colectivo de Gestorxs Culturales almerienses que busca poner en
+    valor la vida cultural de la ciudad y de la provincia desde una visión
+    innovadora, apoyando y fomentando el talento local, el intercambio y la
+    cooperación internacional y sobre todo la implicación y participación
+    ciudadana a través de proyectos artísticos y culturales que promuevan la
+    reflexión de los retos y temas contemporáneos.
   </StyledWho>
 );
 
