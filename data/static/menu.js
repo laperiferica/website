fedcf497a4f91664a8877db450cd8ac943ef9f1f
Javier López Úbeda <jlopezcur@gmail.com>
2020-04-19 00:18:29 +0200

    Reorganized menu

--- data/static/menu.js
+++ data/static/menu.js
@@ -1,13 +1,12 @@
 export default {
   items: [
-    { title: 'Inicio', to: '/' },
+    { title: 'Quiénes somos', to: '/who' },
     { title: 'Proyectos', to: '/projects' },
     {
       title: 'Erasmus+ y CES',
       to: '/programs/erasmus+-y-cuerpo-europeo-de-solidaridad',
     },
-    { title: 'Noticias', to: '/posts' },
-    { title: 'Quienes somos', to: '/who' },
+    // { title: 'Noticias', to: '/posts' },
     { title: 'Amigxs', to: '/friends' },
     { title: 'Contacta', to: '/contact' },
   ],
