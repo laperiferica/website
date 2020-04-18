6f5fa43e14b774bd78f0a39a45da7a1e668174bf
jlopezcur <jlopezcur@users.noreply.github.com>
2020-04-18 22:29:10 +0000

    deploy: b4521accc3fbe7e2030584beafae4dce0145783f

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-2f07b955c5366b77c5d4.js"
+    "url": "webpack-runtime-9c020844a95fab53b5d7.js"
   },
   {
     "url": "styles.8a75dcbbf4db4ede5abe.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "7ef89258c6f990137e0206052f0116e5"
+    "revision": "8f0bc9385f82374fe82462a840f5e68e"
   },
   {
     "url": "google-fonts/s/droidsans/v12/SlGVmQWMvZQIdix7AFxXkHNSbQ.woff2",
@@ -58,7 +58,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "aa1d201f353a9490b36b7f7b2c5bbc0c"
+    "revision": "a63083af944740ec94e01081ae665982"
   },
   {
     "url": "manifest.webmanifest",
