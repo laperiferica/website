a95dbc91d8b4222b38f0c00748081bc7971a4cd2
Javier López Úbeda <jlopezcur@gmail.com>
2020-04-26 23:00:53 +0200

    Removed featured section from home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,16 +6,12 @@ import Layout from '../components/Layout';
 import SEO from '../components/SEO';
 import Section from '../components/Section';
 
-import Featured from '../components/home/Featured';
 import LatestProjects from '../components/home/LatestProjects';
 import Who from '../components/home/Who';
 
 const IndexPage = ({ intl }) => (
   <Layout>
     <SEO title={intl.formatMessage({ id: 'Home' })} />
-    <Section id={'featured'} title={intl.formatMessage({ id: 'Stand out...' })}>
-      <Featured />
-    </Section>
     <Section id={'quienes-somos'} title={'La Periférica Cultura Contemporánea'}>
       <Who />
       <Link to={'/who'}>Conócenos mejor...</Link>
