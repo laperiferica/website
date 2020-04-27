66f3fdc136480926fec84397e571d1e74334eab7
Javier López Úbeda <jlopezcur@gmail.com>
2020-04-27 04:47:37 +0200

    Added menu mobile button aria-label

--- src/components/Menu/Burger.jsx
+++ src/components/Menu/Burger.jsx
@@ -55,7 +55,11 @@ const StyledBurger = styled.button`
 
 const Burger = ({ open, setOpen }) => {
   return (
-    <StyledBurger open={open} onClick={() => setOpen(!open)}>
+    <StyledBurger
+      open={open}
+      onClick={() => setOpen(!open)}
+      aria-label={'menu'}
+    >
       <div />
       <div />
       <div />
