97a87881c452eb4a58e7495097c0dce0d66f7806
Javier López Úbeda <jlopezcur@gmail.com>
2020-04-27 03:50:03 +0200

    Added photo to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { injectIntl } from 'gatsby-plugin-intl';
+import styled from 'styled-components';
 
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
@@ -9,23 +10,33 @@ import Section from '../components/Section';
 import Team from '../components/contact/Team';
 
 import meta from '../../data/static/meta';
+import image from '../images/team.jpg';
+
+const StyledContactPage = styled.div`
+  img {
+    margin: 2rem 0;
+    width: 100%;
+  }
+`;
 
 const ContactPage = ({ intl }) => (
   <Layout>
     <SEO title={intl.formatMessage({ id: 'Contact' })} />
-    <Section id={'email'} title={'Email'}>
-      <a
-        href={`mailto:${meta.email}`}
-        target={'_blank'}
-        rel={'noopener noreferrer'}
-      >
-        {meta.email}
-      </a>
-    </Section>
-    <Section
-      id={'team-contact'}
-      title={intl.formatMessage({ id: 'Team Contact' })}
-    >
+    <StyledContactPage>
+      <Section id={'email'} title={intl.formatMessage({ id: 'Contact' })}>
+        <center>
+          <a
+            href={`mailto:${meta.email}`}
+            target={'_blank'}
+            rel={'noopener noreferrer'}
+          >
+            {meta.email}
+          </a>
+        </center>
+        <img src={image} alt={'Foto del equipo'} />
+      </Section>
+    </StyledContactPage>
+    <Section id={'team-contact'}>
       <Team />
     </Section>
   </Layout>
