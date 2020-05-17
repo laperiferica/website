fd6976e2d287adbeabc9e460d0fc7a10a9f87f43
jlopezcur <jlopezcur@users.noreply.github.com>
2020-05-17 15:15:13 +0000

    deploy: a9b3dc3596151f18f146ee956c33c039f50a64f0

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-26b2e0fc0e367b90b8e6.js"
+    "url": "webpack-runtime-e3dc15c8fb7a200e8665.js"
   },
   {
     "url": "styles.80ed3ce79f5552c47294.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "c87092ffd59d8f757c2664e04c12a85a"
+    "revision": "894fccbaa9b4da183d846ec61bb66d68"
   },
   {
     "url": "google-fonts/s/droidsans/v12/SlGVmQWMvZQIdix7AFxXkHNSbQ.woff2",
