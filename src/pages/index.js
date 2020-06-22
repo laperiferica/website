245e570068a36614a2e9b7ece2610d735be42715
Javier López Úbeda <jlopezcur@gmail.com>
2020-06-22 18:13:13 +0200

    fix: added expo banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,41 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
 import { injectIntl } from 'gatsby-plugin-intl';
+import styled from 'styled-components';
 
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
 import Section from '../components/Section';
 import Grid from '../components/Grid';
 
+import banner from '../../data/featured/current/image.jpg';
+
+const StyledBanner = styled.div`
+  text-align: center;
+  margin-bottom: 4rem;
+  img {
+    width: 80%;
+  }
+
+  @media (max-width: 980px) {
+    img {
+      width: 100%;
+    }
+  }
+`;
+
 const IndexPage = ({ data, intl }) => (
   <Layout>
     <SEO title={intl.formatMessage({ id: 'Home' })} />
+    <StyledBanner>
+      <a
+        href={'https://confines-ilustrados.laperifericacc.com'}
+        target={'_blank'}
+        rel={'noopener noreferrer'}
+      >
+        <img src={banner} />
+      </a>
+    </StyledBanner>
     <Section id={'quienes-somos'} title={'La Periférica Cultura Contemporánea'}>
       <div dangerouslySetInnerHTML={{ __html: data.text.html }} />
     </Section>
